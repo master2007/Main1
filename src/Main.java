350f411b83e706fd8c956b00457337604cbebb2e
Master <aaa@aaa.aa>
2016-07-15 02:04:01 +0600

    komm1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+/**
+ * Created by Master on 15.07.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(new Date());
+    }
+}
